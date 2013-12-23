0d309bdb00b735a4a7ab196051186770974de6f7
Brian Yingling <brian@daft.io>
2013-12-22 21:29:46 -0500

    adding properites to game model

--- models/game.js
+++ models/game.js
@@ -8,11 +8,18 @@ var gameSchema = new Schema({
   title:         String,
   maxNumPlayers: Number,
   players:       Array,
+  patrons:       Array,
   cards:         Array,
   isActive:      {type: Boolean, default: false},
   isWon:         {type: Boolean, default: false},
-  winner:        String,
-  turnToken:     String
+  winner:        {type: String,  default: null},
+  turnToken:     {type: String,  default: null}
 });
+
+gameSchema.methods = {
+  addPatron: function(patron) {
+    this.patrons.push(patron);
+  }
+};
 
 module.exports = mongoose.model('Game', gameSchema);
