52b9e1105bbc28d52ee8f7bd308479f01d1fdcb2
Brian Yingling <brian_yingling@yahoo.com>
2014-01-05 18:18:08 -0500

    uses variables on user obj rather than directly on session obj

--- controllers/home.js
+++ controllers/home.js
@@ -5,5 +5,5 @@
 
 exports.index = function(req, res){
   console.log(req.session);
-  res.render('index', { title: 'Dominion', session: req.session });
+  res.render('index', { title: 'Dominion', user: req.session.user });
 };
